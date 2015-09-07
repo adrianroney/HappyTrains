816b8e43cfe4e4cf633ff632b8d75266fd5faea3
Adrian Roney <adrian.roney@tigerspike.com>
2015-09-07 13:29:06 +0400

    Commit

--- app/src/main/java/happytrains/com/happytrain/MainActivity.java
+++ app/src/main/java/happytrains/com/happytrain/MainActivity.java
@@ -1,17 +1,29 @@
 package happytrains.com.happytrain;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+    private Button mSubmitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+        getActionBar().setBackgroundDrawable(new ColorDrawable(0x00bcd4));
     }
 
 
@@ -19,7 +31,46 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mSubmitButton = (Button)findViewById(R.id.submit_button);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (validationPassess()) {
+                    Intent searchResultsActivity = new Intent(MainActivity.this, SearchResults.class);
+                    startActivity(searchResultsActivity);
+                } else {
+                    showValidationError();
+                }
+            }
+        });
+
         return true;
+    }
+
+    private void showValidationError() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle(R.string.validation_dialog_title);
+        dialogBuilder.setMessage(R.string.validation_message);
+        dialogBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        }).show();
+
+    }
+
+    private boolean validationPassess() {
+        return false;
     }
 
     @Override
