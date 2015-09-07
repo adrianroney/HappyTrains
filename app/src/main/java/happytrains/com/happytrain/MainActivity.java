56ce2e117974bd20cfa8f1b7ff6ef19e5f5a560c
Le Thanh Long <S729799@emirates.com>
2015-09-07 13:30:13 +0400

    add date time picker

--- app/src/main/java/happytrains/com/happytrain/MainActivity.java
+++ app/src/main/java/happytrains/com/happytrain/MainActivity.java
@@ -1,17 +1,50 @@
 package happytrains.com.happytrain;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
-public class MainActivity extends Activity {
+import java.util.Calendar;
+
+public class MainActivity extends Activity implements
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener{
+
+    EditText departureDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        departureDateTime = (EditText) findViewById(R.id.datetime_edit_text);
+        departureDateTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        MainActivity.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.setThemeDark(false);
+                dpd.vibrate(false);
+                dpd.setMinDate(now);
+                Calendar oneyear = Calendar.getInstance();
+                oneyear.add(Calendar.YEAR, 1);
+                dpd.setMaxDate(oneyear);
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
     }
 
 
@@ -20,6 +53,16 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TimePickerDialog tpd = (TimePickerDialog) getFragmentManager().findFragmentByTag("Timepickerdialog");
+        DatePickerDialog dpd = (DatePickerDialog) getFragmentManager().findFragmentByTag("Datepickerdialog");
+
+        if(tpd != null) tpd.setOnTimeSetListener(this);
+        if(dpd != null) dpd.setOnDateSetListener(this);
     }
 
     @Override
@@ -35,5 +78,34 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar now = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(
+                MainActivity.this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                true
+        );
+        tpd.setThemeDark(false);
+        tpd.vibrate(false);
+        tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                departureDateTime.setText("");
+            }
+        });
+        view.dismiss();
+        tpd.show(getFragmentManager(), "Timepickerdialog");
+        String date = dayOfMonth + "/" + monthOfYear + "/" + year;
+        departureDateTime.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hourOfDay, int minute) {
+        String dateTime = departureDateTime.getText() + " " + hourOfDay + ":" + minute;
+        departureDateTime.setText(dateTime);
     }
 }
